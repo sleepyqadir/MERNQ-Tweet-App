d03206a4f64ffadea91f545e24ebd40787bed2ae
AQadir64 <a.qadir67@outlook.com>
2020-06-17 22:09:58 +0500

    add : query mutations

--- client/src/utils/graphql.js
+++ client/src/utils/graphql.js
@@ -38,21 +38,89 @@ export const FETCH_POST_QUERY = gql`
   {
     getPosts {
       id
-      username
       body
       createdAt
-      comments {
-        id
-        body
-        createdAt
+      username
+      likeCount
+      likes {
         username
       }
-      likes {
+      comments {
         id
+        username
         createdAt
+        body
+      }
+      commentCount
+    }
+  }
+`;
+
+export const LIKE_POST_MUTATION = gql`
+  mutation likePost($postId: ID!) {
+    likePost(postId: $postId) {
+      id
+      likes {
         username
       }
       likeCount
+    }
+  }
+`;
+
+export const FETCH_SINGLE_POST_QUERY = gql`
+  query($postId: ID!) {
+    getPost(postId: $postId) {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+      commentCount
+    }
+  }
+`;
+
+export const DELETE_POST_MUTATION = gql`
+  mutation deletePost($postId: ID!) {
+    deletePost(postId: $postId)
+  }
+`;
+
+export const DELETE_COMMENT_MUTATION = gql`
+  mutation deleteComment($postId: ID!, $commentId: ID!) {
+    deleteComment(postId: $postId, commentId: $commentId) {
+      id
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+      commentCount
+    }
+  }
+`;
+
+export const SUBMIT_COMMENT_MUTATION = gql`
+  mutation createComment($postId: ID!, $body: String!) {
+    createComment(postId: $postId, body: $body) {
+      id
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
       commentCount
     }
   }
